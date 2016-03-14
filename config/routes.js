a911a2d5b8eec7772fc59e8fc6f5008227d88454
Karen Quan <quan.karen@gmail.com>
2016-03-14 01:02:02 -0700

    update config/routes.js

--- config/routes.js
+++ config/routes.js
@@ -4,12 +4,23 @@ var express = require('express'),
 // Require controllers.
 var pagesController = require('../controllers/pages');
 var usersController = require('../controllers/users');
+var charactersController = require('../controllers/characters');
+var comicsController = require('../controllers/comics');
 
 // root path:
-router.get('/', pagesController.welcome);
+router.get('/', pagesController.index);
 
 // users resource paths:
 router.get('/users',     usersController.index);
 router.get('/users/:id', usersController.show);
+
+// characters
+router.get('/characters', charactersController.index);
+router.post('/characters/create', charactersController.create);
+router.get('/characters/:id', charactersController.show);
+router.get('/characters/search/:name', charactersController.search);
+
+// comics
+router.get('/comics/:id', comicsController.show);
 
 module.exports = router;
