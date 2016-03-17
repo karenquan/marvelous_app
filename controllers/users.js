e7a55dc0bbae793754b0c7904de82aa7b28ef88a
Karen Quan <quan.karen@gmail.com>
2016-03-16 19:15:10 -0700

    add back index in users controller (accidentally deleted it in last commit)

--- controllers/users.js
+++ controllers/users.js
@@ -3,16 +3,18 @@ module.exports = {
   index: index,
   show:  show
 };
+function index(req, res, next) {
 
+}
 function show(req, res, next) {
   User.find({ facebookId: req.params.id }, function(err, user) {
     var _user = user[0];
     if (err) {
-      res.json({ message: 'Could not find user because ' + err});
+      res.json({ message: 'Could not find user because ' + err });
     } else if (!user) {
-      res.json({ message: 'No user with this id.'});
+      res.json({ message: 'No user with this id.' });
     } else {
-      res.render('users/show', {user: _user});
+      res.render('users/show', { user: _user });
     }
   });
 }
